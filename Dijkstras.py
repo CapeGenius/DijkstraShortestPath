1fbe25528dbf3e2bd2e89a4f858c0d22a075d807
CapeGenius <90223034+CapeGenius@users.noreply.github.com>
2023-01-29 16:50:36 -0600

    Dijkstra Algorithm Major Update
    I updated my Dijkstra algorithm to mimic a "priority queue" insertion of nodes into the pathsUnvisited (or the remaining paths). Moreover, I changed the table and graph data structures into dictionaries as I could more easily search for node values instead of searching an entire list. I was very proud of myself as I discovered a solution based on my own understanding rather than using the internet.

--- Dijkstras.py
+++ Dijkstras.py
@@ -1,52 +1,73 @@
 # Rohan Bendapudi - 1/24/2023
+# I converted 
+class Graph: # graph data structure with all the nodes and weights, table, paths visited, paths unvisited, and the source of the graph
 
-class Node:
-    index = None
-    connectedNodes = ()
-
-    def _init_(index):
-        self.index = index
-class Dijkstra:
-    
+    graph = {}
+    table = {}
     pathsVisited = []
     pathsUnvisited = []
+    source = int
 
-
-    def declareNodes():
-        node1 = Node(1)
-        node2 = Node(2)
-        node3 = Node(3)
-        node4 = Node(4)
-        node6 = Node(6)
-        node7 = Node(7)
-        node8 = Node(8)
-        node10 = Node(10)
-        node1.connectedNodes = ([node3,2],[node2,4],[node7,7])
-        node2.connectedNodes = ([node1,4],[node4,2])
-        node3.connectedNodes = ([node1,2],[node7,3],[node6,8])
-        node4.connectedNodes = ([node2,2],[node7,5],[node8,6])
-        node6.connectedNodes = ([node3,8],[node10,3])
-        node7.connectedNodes = ([node3,3],[node1,7],[node10,4],[node4,5])
-        node8.connectedNodes = ([node10,2],[node4,6])
-        node10.connectedNodes = ([node8,2],[node6,3],[node7,4])
-
-        self.pathsUnvisited = [node1, node2, node3, node4, node6, node7, node8, node10]
+    def __init__(self):
+        self.graph = {}
+        self.table = {}
         self.pathsVisited = []
+        self.pathsUnvisited = []
+        self.source = int
 
-    def DijkstraPolicy():
+    def declareNodes(self): # declares all the nodes in order of indices
+        self.graph[0] = [[1,4],[2,2],[4,7]]
+        self.graph[1] = [[0,4],[3,2]]
+        self.graph[2] = [[0,2],[4,3],[5,8]]
+        self.graph[3] = [[1,2],[4,5],[6,6]]
+        self.graph[4] = [[0,7],[2,3],[3,5],[7,4]]
+        self.graph[5] = [[7,3],[2,8]]
+        self.graph[6] = [[3,6],[7,7]]
+        self.graph[7] = [[6,7],[5,3],[4,4]]
         
-        allVisited = False
+        print(self.graph)
 
-        while allVisited == False:
-            if self.pathsUnvisited.size() == 0:
-                allVisited = True
+    # sets the starting node of the graph and loads the table as well as paths unvisited
+    def setSource(self, index):
+        self.source = index
+        for x in range(len(self.graph)):
+            if (index == x):
+                self.table[x] = 0 #adds to dictionary with value of zero
+                self.pathsUnvisited.insert(0, x) # adds the node to the front if its the source
             else:
-                pass
-            pass
+                self.table[x] = 100000000000 # adds to dictionary with an extremely high value
+                self.pathsUnvisited.append(x) # adds node normally if it's not the source
+        print(self.pathsUnvisited)
+        print(self.table)
+    
+    def orderNode(self, nodeValue, dist): # mimics "priority" in queue to insert in proper location based on length
+        for x in range(len(self.pathsUnvisited)):
+            if (self.table[self.pathsUnvisited[x]] > dist):
+                self.pathsUnvisited.insert(x, nodeValue) # inserts the value of the node in its proper location
 
+    def nodeWorks(self, nodeValue, currentNode):
+        for x in range(len(currentNode)): # updates the table of nodes based on distances
+            # calculates the local distance to a node + distance to get to that node
+            distance = currentNode[x][1] + self.table[nodeValue]
+            if (distance < self.table[currentNode[x][0]]): 
+                self.table[currentNode[x][0]] = distance # updates the distnace to get to that node
+                self.pathsUnvisited.remove(nodeValue)
+                self.orderNode(nodeValue, distance) #calls a method to reorder the nodes
 
-
-        pass
+    def dijkstraPath(self, goal): # main algorithm that finds the shortest path
+        
+        while (len(self.pathsUnvisited) != 0):
+            node = self.pathsUnvisited[0]
+            self.nodeWorks(node, self.graph[node])
+            self.pathsVisited.append(node)
+            del self.pathsUnvisited[0]
+                    
+        print(self.table)
 
 def main():
-    pass
+    dijkstraGraph = Graph()
+    dijkstraGraph.declareNodes()
+    dijkstraGraph.setSource(0)
+    dijkstraGraph.dijkstraPath(2)
+
+main()
